eef661c68785e9b59c221f26d986f29889009acf
Amit Vyas <23amitvyas@gmail.com>
2024-07-09 18:46:14 -0400

    removing code from home.js

--- react_frontend/src/Home.js
+++ react_frontend/src/Home.js
@@ -1,15 +1,1 @@
 import Profile from "./Profile.js";
-
-function Home() {
-    return (
-      <section>
-        <h1>Amazing scientists</h1>
-        <ul>
-          <li> <Profile /> </li>
-        </ul>
-        
-      </section>
-    );
-  }
-
-  export default Home;
